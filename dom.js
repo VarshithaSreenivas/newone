8afc18d16b90efd67e043963a57af0d82bda72ba
VarshithaSreenivas <varshithasompalle@gmail.com>
2023-09-15 18:38:30 +0530

    second

--- dom.js
+++ dom.js
@@ -7,10 +7,17 @@ headerTitle.innerHTML='<h4>Hii</h4>';
 var header=document.getElementById('main-header');
 header.style.borderBottom='solid 4px #000';
 
-var items=document.getElementsByClassName('title');
+//var items=document.getElementsByClassName('title');
+//console.log(items);
+//items[0].style.fontWeight='bold';
+//items[0].style.color="green";
+var items=document.getElementsByClassName('list-group-item');
 console.log(items);
+items[2].style.backgroundColor="green";
 items[0].style.fontWeight='bold';
-items[0].style.color="green";
+items[1].style.fontWeight='bold';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
 
 
 
